c051a59a538b838ec2970ea663709b981ee5375c
ja1cap <ja1cap@me.com>
2014-07-03 11:49:16 +0300

    AbstractEntity subclass

---
+++ src/Weasty/Bundle/DoctrineBundle/Entity/AbstractEntity.php
@@ -0,0 +1,12 @@
+<?php
+namespace Weasty\Bundle\DoctrineBundle\Entity;
+
+use Weasty\Doctrine\Entity\AbstractEntity as BaseEntity;
+
+/**
+ * Class AbstractEntity
+ * @package Weasty\Bundle\DoctrineBundle\Entity
+ */
+abstract class AbstractEntity extends BaseEntity {
+
+} 
