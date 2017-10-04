779a50900b9f8f33b0ea6871ba8b7dbcf24a4be0
ytsialitsyn <ytsialitsyn@scnsoft.com>
2017-10-04 12:31:32 +0300

    EntityType

--- src/Weasty/Bundle/DoctrineBundle/Form/Type/EntityType.php
+++ src/Weasty/Bundle/DoctrineBundle/Form/Type/EntityType.php
@@ -1,102 +1,100 @@
 <?php
+
 namespace Weasty\Bundle\DoctrineBundle\Form\Type;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityLoaderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\DoctrineType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Weasty\Bundle\DoctrineBundle\Form\ChoiceList\EntityLoader;
 use Weasty\Bundle\DoctrineBundle\Form\DataTransformer\IdToEntityTransformer;
 
 /**
  * Class EntityType
+ *
  * @package Weasty\Bundle\DoctrineBundle\Form\Type
  */
 class EntityType extends DoctrineType {
 
-    /**
-     * @var string
-     */
-    protected $entityClass;
+  /**
+   * @var string
+   */
+  protected $entityClass;
 
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+  /**
+   * @inheritdoc
+   */
+  public function configureOptions( OptionsResolver $resolver ) {
 
-        parent::setDefaultOptions($resolver);
+    parent::configureOptions( $resolver );
 
-        $resolver
-            ->setDefaults(array(
-                'map_as_id' => false,
-                'class' => $this->getEntityClass(),
-            ))
-        ;
+    $resolver->setDefaults(
+      [
+        'map_as_id' => false,
+        'class'     => $this->getEntityClass(),
+      ]
+    );
 
-    }
+  }
 
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+  /**
+   * @param FormBuilderInterface $builder
+   * @param array                $options
+   */
+  public function buildForm( FormBuilderInterface $builder, array $options ) {
 
-        parent::buildForm($builder, $options);
+    parent::buildForm( $builder, $options );
 
-        if($options['map_as_id']){
+    if( $options[ 'map_as_id' ] ) {
 
-            $builder
-                ->addModelTransformer(new IdToEntityTransformer($this->registry->getManager(), $options['class']))
-            ;
-
-        }
+      $builder->addModelTransformer( new IdToEntityTransformer( $this->registry->getManager(), $options[ 'class' ] ) );
 
     }
 
-    /**
-     * Return the default loader object.
-     *
-     * @param ObjectManager $manager
-     * @param mixed $queryBuilder
-     * @param string $class
-     *
-     * @return EntityLoaderInterface
-     */
-    public function getLoader(ObjectManager $manager, $queryBuilder, $class)
-    {
+  }
 
-        $repository = $manager->getRepository($class ?: $this->getEntityClass());
-        return new EntityLoader($queryBuilder, $repository);
+  /**
+   * Return the default loader object.
+   *
+   * @param ObjectManager $manager
+   * @param mixed         $queryBuilder
+   * @param string        $class
+   *
+   * @return EntityLoaderInterface
+   */
+  public function getLoader( ObjectManager $manager, $queryBuilder, $class ) {
 
-    }
+    $repository = $manager->getRepository( $class ?: $this->getEntityClass() );
 
-    /**
-     * @param string $entityClass
-     */
-    public function setEntityClass($entityClass)
-    {
-        $this->entityClass = $entityClass;
-    }
+    return new EntityLoader( $queryBuilder, $repository );
 
-    /**
-     * @return string
-     */
-    public function getEntityClass()
-    {
-        return $this->entityClass;
-    }
+  }
 
-    /**
-     * Returns the name of this type.
-     *
-     * @return string The name of this type
-     */
-    public function getName()
-    {
-        return 'weasty_doctrine_entity_type';
-    }
+  /**
+   * Returns the name of this type.
+   *
+   * @return string The name of this type
+   */
+  public function getName() {
+
+    return 'weasty_doctrine_entity_type';
+  }
+
+  /**
+   * @return string
+   */
+  public function getEntityClass() {
+
+    return $this->entityClass;
+  }
+
+  /**
+   * @param string $entityClass
+   */
+  public function setEntityClass( $entityClass ) {
+
+    $this->entityClass = $entityClass;
+  }
 
 }
